cb48960b27f14db1b3fca9d72cc5eb1d25aac7c7
CristianEscequiel <esce.arguello21@gmail.com>
2026-08-04 11:37:25 -0300

    feat(component): update button component

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
-export class Button {}
+export class Button {
+
+  clicked = output<void>();
+  variant = input<'primary' | 'secondary' | 'outline' | 'danger'>('primary');
+
+  classes = computed(() => ({
+    'btn--primary': this.variant() === 'primary',
+    'btn--secondary': this.variant() === 'secondary',
+    'btn--outline': this.variant() === 'outline',
+    'btn--danger': this.variant() === 'danger',
+  }));
+
+  onClick(): void {
+    this.clicked.emit();
+  }
+
+}
